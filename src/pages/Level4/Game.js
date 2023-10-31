f7c6671253d9859d7fed3467e3d8e00bffd9c212
shubhanshu-0 <shubhanshusaraf@SHUBHANSHUs-MacBook-Pro.local>
2023-10-31 12:09:55 +0530

    ok

--- src/pages/Level4/Game.js
+++ src/pages/Level4/Game.js
@@ -50,32 +50,32 @@ function Game() {
   const [pathWidths, setPathWidths] = useState(
     // Initialize path widths as an array
     [
-      { top: 570, left: 750, width: 65, height: 120 , type : 1,speed : 'fast' , no:'1'},
-      { top: 432, left: 750, width: 65, height: 120 , type : 1,speed : 'fast' , no:'2'},
-      { top: 570, left: 550, width: 200, height: 60 , type : 2,speed : 'slow', no:'3'},
-      { top: 570, left: 815, width: 115, height: 60 , type : 1,speed : 'fast' , no:'4'},
-      { top: 512, left: 605, width: 145, height: 40 , type : 2,speed : 'fast',no:'5'},
-      { top: 280, left: 600, width: 50, height: 220 , type : 2,speed : 'fast',no:'6'},
-      { top: 320, left: 650, width: 200, height: 50 , type : 2,speed : 'slow',no:'7'},
-      { top: 530, left: 405, width: 200, height: 40 , type : 1,speed : 'fast',no:'8'},
-      { top: 570, left: 405, width: 70, height: 60 , type : 2,speed : 'fast',no:'9'},
-      { top: 330, left: 540, width: 60, height: 100 , type : 2,speed : 'fast',no:'10'},
-      { top: 310, left: 480, width: 60, height: 120 , type : 2,speed : 'fast',no:'11'},
-      { top: 432, left: 650, width: 100, height: 50 , type : 1,speed : 'fast',no:'12'},
-      { top: 630, left: 325, width: 300, height: 50 , type : 2,speed : 'fast',no:'13'},
-      { top: 440, left: 435, width: 165, height: 60 , type : 2,speed : 'fast',no:'14'},
-      { top: 500, left: 435, width: 50, height: 30 , type : 1,speed : 'fast',no:'15'},
-      { top: 480, left: 335, width: 50, height: 150 , type : 2,speed : 'fast',no:'16'},
-      { top: 380, left: 335, width: 100, height: 100 , type : 2,speed : 'fast',no:'17'},
-      { top: 310, left: 335, width: 50, height: 70 , type : 2,speed : 'fast',no:'18'},
-      { top: 260, left: 335, width: 150, height: 50 , type : 2,speed : 'fast',no:'19'},
-      { top: 280, left: 945, width: 70, height: 260 , type : 1,speed : 'slow',no:'20'},
-      { top: 210, left: 795, width: 220, height: 70 , type : 1,speed : 'fast',no:'21'},
-      { top: 280, left: 750, width: 100, height: 40 , type : 2,speed : 'slow',no:'22'},
-      { top: 230, left: 650, width: 145, height: 50 , type : 2,speed : 'fast',no:'23'},
-      { top: 215, left: 580, width: 70, height: 65 , type : 2,speed : 'fast',no:'24'},
-      { top: 200, left: 455, width: 125, height: 50 , type : 2,speed : 'fast',no:'25'},
-      { top: 370, left: 770, width: 175, height: 62 , type : 2,speed : 'fast',no:'26'},
+      { top: 570-60, left: 750-80, width: 65, height: 120 , type : 1,speed : 'fast' , no:'1'},
+      { top: 432-60, left: 750-80, width: 65, height: 120 , type : 1,speed : 'fast' , no:'2'},
+      { top: 570-60, left: 550-80, width: 200, height: 60 , type : 2,speed : 'slow', no:'3'},
+      { top: 570-60, left: 815-80, width: 115, height: 60 , type : 1,speed : 'fast' , no:'4'},
+      { top: 512-60, left: 605-80, width: 145, height: 40 , type : 2,speed : 'fast',no:'5'},
+      { top: 280-60, left: 600-80, width: 50, height: 220 , type : 2,speed : 'fast',no:'6'},
+      { top: 320-60, left: 650-80, width: 200, height: 50 , type : 2,speed : 'slow',no:'7'},
+      { top: 530-60, left: 405-80, width: 200, height: 40 , type : 1,speed : 'fast',no:'8'},
+      { top: 570-60, left: 405-80, width: 70, height: 60 , type : 2,speed : 'fast',no:'9'},
+      { top: 330-60, left: 540-80, width: 60, height: 100 , type : 2,speed : 'fast',no:'10'},
+      { top: 310-60, left: 480-80, width: 60, height: 120 , type : 2,speed : 'fast',no:'11'},
+      { top: 432-60, left: 650-80, width: 100, height: 50 , type : 1,speed : 'fast',no:'12'},
+      { top: 630-60, left: 325-80, width: 300, height: 50 , type : 2,speed : 'fast',no:'13'},
+      { top: 440-60, left: 435-80, width: 165, height: 60 , type : 2,speed : 'fast',no:'14'},
+      { top: 500-60, left: 435-80, width: 50, height: 30 , type : 1,speed : 'fast',no:'15'},
+      { top: 480-60, left: 335-80, width: 50, height: 150 , type : 2,speed : 'fast',no:'16'},
+      { top: 380-60, left: 335-80, width: 100, height: 100 , type : 2,speed : 'fast',no:'17'},
+      { top: 310-60, left: 335-80, width: 50, height: 70 , type : 2,speed : 'fast',no:'18'},
+      { top: 260-60, left: 335-80, width: 150, height: 50 , type : 2,speed : 'fast',no:'19'},
+      { top: 280-60, left: 945-80, width: 70, height: 260 , type : 1,speed : 'slow',no:'20'},
+      { top: 210-60, left: 795-80, width: 220, height: 70 , type : 1,speed : 'fast',no:'21'},
+      { top: 280-60, left: 750-80, width: 100, height: 40 , type : 2,speed : 'slow',no:'22'},
+      { top: 230-60, left: 650-80, width: 145, height: 50 , type : 2,speed : 'fast',no:'23'},
+      { top: 215-60, left: 580-80, width: 70, height: 65 , type : 2,speed : 'fast',no:'24'},
+      { top: 200-60, left: 455-80, width: 125, height: 50 , type : 2,speed : 'fast',no:'25'},
+      { top: 370-60, left: 770-80, width: 175, height: 62 , type : 2,speed : 'fast',no:'26'},
     ]
   );
   //type 1 -- width short (right -  short)
@@ -97,7 +97,7 @@ function Game() {
           mouseY >= path.top &&
           mouseY <= path.top + path.height
         );
-        if ((mouseX < 930 || mouseX > 1080 ||  mouseY < 540 || mouseY > 710) && (mouseX < 395 || mouseX > 460 || mouseY < 180 || mouseY > 260 ) && !isCursorInPath) {
+        if ((mouseX < 850 || mouseX > 1100 ||  mouseY < 480 || mouseY > 720) && (mouseX < 315 || mouseX > 375 || mouseY < 120 || mouseY > 200 ) && !isCursorInPath) {
           decreaseScore(10);
           setIsGameOver(true);
         }
